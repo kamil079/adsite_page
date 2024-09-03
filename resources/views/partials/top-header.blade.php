c63a826446ae26a1ee8382bc74c916e16c28728b
kamil079 <kamil@nextgm.ca>
2024-09-03 19:31:48 +0200

    hot fix

--- resources/views/partials/top-header.blade.php
+++ resources/views/partials/top-header.blade.php
@@ -2,7 +2,7 @@
     <div class='relative container px-6 flex flex-col gap-4 justify-between py-4 text-customBlack tablet:flex-row text-[15px] tablet:px-[50px] tablet:items-center'>
         <div class='hidden hd:block bg-[url("/public/images/hero/line.webp")] bg-cover bg-no-repeat absolute left-[45%] top-0 z-10 w-[600px] h-[600px] '></div>
         <div class='hidden hd:block bg-[url("/public/images/hero/line-2.webp")] bg-cover bg-no-repeat absolute left-[45%] top-0 z-10 w-[800px] h-[600px] '></div>
-        <img loading='lazy' src="/images/logo/logo.webp" alt="" class='max-w-[155px] h-[30px]'>
+        <img loading='lazy' src="/images/logo/logo.png" alt="" class='max-w-[155px] h-[30px]'>
         <div class='hidden tablet:flex items-center gap-6 font-normal roboto-flex '>
             <p class='cursor-pointer'>Galeria zdjęć</p>
             <p class='cursor-pointer'>FaQ</p>
